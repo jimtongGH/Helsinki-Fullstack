cbd42d0796defd5b9f7747d0e020730ca1e568f8
Jim <294862048a@gmail.com>
2025-07-14 20:23:15 +0200

    part2 2.1-2.5

--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from 'react-dom/client'
 
 // courseinfo exercise 1.1-1.5
-// import App from '../courseinfo/exercise.jsx'
+// import App from '../courseinfo/App.jsx'
 
 //unicafe exercise 1.6-1.11
-// import App from'../unicafe/exercise.jsx'
+// import App from'../unicafe/App.jsx'
 
 // anecdotes exercise 1.12-1.14
 import App from'../anecdotes/exercise.jsx'
---
+++ part2/src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+
+// courseinfo exercise 2.1-1.5
+import App from '../courseinfo/src/App.jsx'
+
+//unicafe exercise 1.6-1.11
+// import App from'../unicafe/App.jsx'
+
+// anecdotes exercise 1.12-1.14
+// import App from'../anecdotes/App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
